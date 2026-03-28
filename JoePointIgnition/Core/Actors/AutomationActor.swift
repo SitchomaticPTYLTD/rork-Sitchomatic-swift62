beacb20b4ce94cad2fda488335481b9c1e39493f
Maconick <daleyztt@gmail.com>
2026-03-28 20:54:27 +1100

    Revert "Refactor to JoePointIgnition MVVM-Service architecture with Swift 6.2 concurrency"

--- JoePointIgnition/Core/Actors/AutomationActor.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-/// Dedicated global actor that isolates all automation TaskGroup orchestration
-/// and DOM parsing off the @MainActor, preventing UI thread contention.
-@globalActor
-actor AutomationActor {
-    static let shared = AutomationActor()
-}
